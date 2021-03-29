07a2929353cf1f2c9d2d044dd0ff00ac980a1e4f
Djordje Turjacanin <etfbl.pisio@gmail.com>
2021-03-29 22:06:26 +0200

    Initial project

---
+++ src/main/java/org/unibl/etf/pisio/am/AmApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.pisio.am;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/unibl/etf/pisio/am/AmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.pisio.am;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
